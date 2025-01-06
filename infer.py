bb67c1461492d7bed27cf5dc3da39915b2881440
Rohit Amarnath <44043399+rohitamar@users.noreply.github.com>
2025-01-05 20:06:22 -0500

    Update infer.py

--- infer.py
+++ infer.py
@@ -21,7 +21,7 @@ print(nplm_model)
 
 from sampling import greedy_decode, random_decode, beam_search
 
-with open('/content/drive/MyDrive/datasets/shakespeare/data.txt') as f:
+with open('./dataset/data.txt') as f:
     data = f.read()
     vocab = sorted(set(data))
     ctoi = {x:i for i, x in enumerate(vocab)}
